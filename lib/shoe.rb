d89ae1e0259b24d6691a735462505ab3d7ef8bd5
Eric Clark <eclark0013@gmail.com>
2019-06-21 14:59:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,12 +13,12 @@ class Shoe
 
   BRANDS=["Uggs"]
 
-#  def brand=(brand)
-#    @brand=brand
+  def brand=(brand)
+    @brand=brand
   #  if !(BRANDS.include?(self.brand))
-#      BRANDS<<brand
+    BRANDS<<brand
   #  end
-#  end
+  end
 
 
 end
